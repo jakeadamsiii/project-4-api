d7ca93d2e953876e0fc5466853ed75ea68c163ca
Jake Adams <jadams.html@gmail.com>
2017-04-15 14:13:14 +0100

    auth done

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Project4Api
+module Fundraiser
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -25,11 +25,14 @@ module Project4Api
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+
+
 config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
